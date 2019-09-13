1998c8a08f763c9c447bd1c071069c562a4ffdbd
st36251524009 <52314794+st36251524009@users.noreply.github.com>
2019-09-13 17:00:50 +0700

    Add files via upload

---
+++ assign.py.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a	=	8 
+b	=	4
+a	+=	b
+print("Subtract&Assign:\t","a=",a,"(12-4)") 
+a	*=	b 
+print("Multiply&Assign:\t","a=",a,"(8×4)") 
+a	/=	b 
+print("Divide&Assign:\t","a=",a,"(32÷4)") 
+a	%=	b 
+print("Modulus&Assign:\t","a=",a,"(8%4)")
+
+
+# In[ ]:
+
+
+
+
---
+++ semantic.py
@@ -0,0 +1,5 @@
+print(	"Python	in easy	steps"	)
+title	=	"Python	in	easy	steps"
+print(	"titel"	)
+num	=	3 
+print(num *(8+4))
